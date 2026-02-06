018bf383f5d26568b3a90f188502d697d1c18fc1
Mahfuz Hasan <mahfuzhasan587@gmail.com>
2026-02-06 22:44:07 +0600

    feat:theming

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,8 @@ type InputProps = React.ComponentProps<"input"> & {
   endIcon?: React.ReactNode;
   classes?: {
     root?: string;
-    icon?: string;
+    startIcon?: string;
+    endIcon?: string;
   };
 };
 
@@ -17,8 +18,8 @@ function Input({ classes, startIcon, endIcon, type, ...props }: InputProps) {
       {startIcon && (
         <span
           className={cn(
-            "pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground [&_svg]:size-4",
-            classes?.icon,
+            "pointer-events-none absolute left-3 top-[55%] -translate-y-1/2 text-muted-foreground [&_svg]:size-4",
+            classes?.startIcon,
           )}
         >
           {startIcon}
@@ -28,8 +29,8 @@ function Input({ classes, startIcon, endIcon, type, ...props }: InputProps) {
         type={type}
         data-slot="input"
         className={cn(
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-11 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "hover:shadow-sm focus:shadow-sm",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           startIcon && "pl-9",
           endIcon && "pr-9",
@@ -41,7 +42,7 @@ function Input({ classes, startIcon, endIcon, type, ...props }: InputProps) {
         <span
           className={cn(
             "pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground [&_svg]:size-4",
-            classes?.icon,
+            classes?.endIcon,
           )}
         >
           {endIcon}
--- src/components/ui/shared/header/header-meta/mode-toggle.tsx
+++ src/components/ui/shared/header/header-meta/mode-toggle.tsx
@@ -16,7 +16,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="rounded-full size-8 shadow-2xl">
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
