8a9eeb00be1ea8d3c31327cce172fc06140940da
Mahfuz Hasan <mahfuzhasan587@gmail.com>
2026-02-26 00:32:16 +0600

    best seller

--- src/app/[locale]/men/gym/components/gym-best-sellers/index.tsx
+++ src/app/[locale]/men/gym/components/gym-best-sellers/index.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/ui/button";
 import SellerLists from "./seller-list";
 
 const GymBestSeller = () => {
   return (
     <div className="section-spacing container">
-      <p className="header-subtitle pb-6 ">BEST SELLERS</p>
+      <div className="flex items-center justify-between">
+        <p className="header-subtitle pb-6 ">BEST SELLERS</p>
+        <Button
+          variant="link"
+          component="link"
+          href="#"
+          className="p-0 h-fit hover:border-none border-none hover:text-tertiary-foreground"
+        >
+          View All
+        </Button>
+      </div>
       <SellerLists />
     </div>
   );
--- src/app/[locale]/men/gym/components/gym-best-sellers/seller-list.tsx
+++ src/app/[locale]/men/gym/components/gym-best-sellers/seller-list.tsx
@@ -1,15 +1,45 @@
 "use client";
 
 import BestSellerCard from "@/app/[locale]/components/best-seller-card";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, HashNavigation, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { menBestSellers } from "../constants";
 
 const SellerLists = () => {
   return (
-    <div className="grid grid-cols-5 gap-5">
+    <Swiper
+      draggable
+      freeMode={true}
+      grabCursor={true}
+      navigation={true}
+      modules={[FreeMode, Navigation, HashNavigation]}
+      hashNavigation={{
+        watchState: true,
+      }}
+      breakpoints={{
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      }}
+    >
       {menBestSellers.map((category) => (
-        <BestSellerCard key={category.id} cardItem={category} />
+        <SwiperSlide key={category.id}>
+          <BestSellerCard cardItem={category} />
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 };
 
